da4237974ade4de7c171c56496003c444313b998
ikramarfaoui123 <arfaouiikram004@gmail.com>
2024-12-20 00:27:29 +0100

    First Commit

---
+++ backend/routes/auth.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const router = express.Router();
+
+// Inscription
+router.post('/signup', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const userExists = await User.findOne({ username });
+        if (userExists) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        const user = new User({ username, password });
+        await user.save();
+        res.status(201).json({ message: 'User created' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+// Connexion
+router.post('/signin', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid username or password' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid username or password' });
+        }
+
+        const token = jwt.sign({ userId: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
+        res.status(200).json({ message: 'Success login', token });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+module.exports = router;
